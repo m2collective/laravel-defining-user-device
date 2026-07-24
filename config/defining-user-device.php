b032423999bb6637200f8d7addc1c2b7b12698eb
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-07-24 10:23:42 +0300

    Create defining-user-device.php

---
+++ config/defining-user-device.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Directives
+    |--------------------------------------------------------------------------
+    */
+
+    'directives' => [
+
+        'definingUserDesktopDevice' => [
+            'openingTag' => 'openingDefiningUserDesktopDevice',
+            'logicalTag' => 'logicalDefiningUserDesktopDevice',
+            'closingTag' => 'closingDefiningUserDesktopDevice',
+        ],
+
+        'definingUserMobileDevice' => [
+            'openingTag' => 'openingDefiningUserMobileDevice',
+            'logicalTag' => 'logicalDefiningUserMobileDevice',
+            'closingTag' => 'closingDefiningUserMobileDevice',
+        ],
+
+        'definingUserTabletDevice' => [
+            'openingTag' => 'openingDefiningUserTabletDevice',
+            'logicalTag' => 'logicalDefiningUserTabletDevice',
+            'closingTag' => 'closingDefiningUserTabletDevice',
+        ],
+
+    ]
+
+];
